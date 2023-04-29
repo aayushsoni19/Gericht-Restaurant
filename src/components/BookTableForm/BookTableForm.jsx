a63eb235b6355af0ffbbb2871d872de3472b4d4a
Hrushikesh41 <hrushikesh.kok@gmail.com>
2023-04-29 14:36:30 +0530

    Validations Set Up Completed

--- src/components/BookTableForm/BookTableForm.jsx
+++ src/components/BookTableForm/BookTableForm.jsx
@@ -1,16 +1,30 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import "./BookTableForm.css";
 import { useNavigate } from "react-router-dom";
 
 const BookTableForm = () => {
     const [details, setDetails] = useState({
-        firstName : "",
-        lastName : "",
-        contact : "",
-        date : "",
-        time : ""
-        });
+        firstName: "",
+        lastName: "",
+        contact: "",
+        date: "",
+        time: ""
+    });
     const navigate = useNavigate();
+
+    const handleChange = (e) => {
+        setDetails({ ...details, [e.target.name]: e.target.value })
+    }
+
+    const handleSubmit = (e) => {
+
+        if (details.firstName == " " || details.lastName == " " || details.contact == " " || details.date == " " || details.time == " ") {
+            alert("Please Enter All Fields")
+        } else {
+            alert("Your Table is Booked...");
+            navigate("/", { replace: true })
+        }
+    }
 
     return (
         <div class="background">
@@ -24,22 +38,22 @@ const BookTableForm = () => {
                     </div>
                     <form class="card-form">
                         <div class="input">
-                            <input type="text" class="input-field" placeholder='First Name'  required />
+                            <input type="text" name='firstName' class="input-field" placeholder='First Name' required onChange={handleChange} />
                         </div>
                         <div class="input">
-                            <input type="text" class="input-field" placeholder='Last Name' required />
+                            <input type="text" name='lastName' class="input-field" placeholder='Last Name' required onChange={handleChange} />
                         </div>
                         <div class="input">
-                            <input type="text" class="input-field" placeholder='Phone Number' required />
+                            <input type="number" name='contact' class="input-field" placeholder='Phone Number' required onChange={handleChange} />
                         </div>
                         <div class="input">
-                            <input type="date" class="input-field" required />
+                            <input type="date" name='date' class="input-field" required onChange={handleChange} />
                         </div>
                         <div class="input">
-                            <input type="time" class="input-field" required />
+                            <input type="time" name='time' class="input-field" required onChange={handleChange} />
                         </div>
                         <div class="action">
-                            <button class="action-button" onClick={() => {alert("Your Table is Booked..."); navigate("/", { replace: true })}}>Book Table</button>
+                            <button class="action-button" onClick={handleSubmit}>Book Table</button>
                         </div>
                     </form>
                 </div>
